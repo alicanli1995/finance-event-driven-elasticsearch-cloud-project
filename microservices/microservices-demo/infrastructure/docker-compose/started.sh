e19e207d70294b85cc70c57f6261b31b745834af
Ali CANLI <alicanli1995@gmail.com>
2022-08-08 23:47:54 +0300

    Small bug fixed

---
+++ microservices/microservices-demo/infrastructure/docker-compose/started.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# started.sh
+
+apt-get update -y
+
+yes | apt-get install curl
+
+curlResult=$(curl -s -o /dev/null -I -w "%{http_code}" http://config-server:8888/actuator/health)
+
+echo "result status code:" "$curlResult"
+
+while [[ ! $curlResult == "200" ]]; do
+  >&2 echo "Config server is not up yet!"
+  sleep 2
+  curlResult=$(curl -s -o /dev/null -I -w "%{http_code}" http://config-server:8888/actuator/health)
+done
+
+./cnb/process/web
